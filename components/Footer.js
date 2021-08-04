6ac6dfaf884629e34e01b0a81a52ac6be4710bba
TheCryptoHub <segre.fernando@gmail.com>
2021-08-04 14:46:45 -0300

    day one build

---
+++ components/Footer.js
@@ -0,0 +1,44 @@
+function Footer() {
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-4 space-y-10 px-32 py-14 bg-gray-100 text-gray-600">
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">ABOUT</h5>
+                <p>How it works</p>
+                <p>Newsroom</p>
+                <p>Investors</p>
+                <p>Airbnb Plus</p>
+                <p>Airbnb Luxe</p>
+            </div>
+            
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">Community</h5>
+                <p>Accessibility</p>
+                <p>This is not a real site</p>
+                <p>It's a pretty awesome clone</p>
+                <p>Referrals accepted</p>
+                <p>Feel free to share</p>
+            </div>
+
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">HOST</h5>
+                <p>Fernando Segre</p>
+                <p>Presents</p>
+                <p>IT Solutions</p>
+                <p>Solving Business Needs</p>
+                <p>Custom Service</p>
+            </div>
+
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">Support</h5>
+                <p>Help Center</p>
+                <p>Trust & Safety</p>
+                <p>Say Hi LinkedIn</p>
+                <p>Easter Eggs</p>
+                <p>For the Win</p>
+            </div>
+
+        </div>
+    )
+}
+
+export default Footer
